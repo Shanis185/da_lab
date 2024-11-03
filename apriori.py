e6ab44a1d4342c85fbd6260c97a44245c0546aea
Shanis185 <shaniswrk2003@gmail.com>
2024-11-03 12:46:52 +0530

    final completed code

--- apriori.py
+++ apriori.py
@@ -2,43 +2,51 @@ import csv
 from itertools import combinations
 
 
-
-def read_file(aprio.csv):
-    data=[]
-    with open(aprio.csv,'r') as f:
-        reader=csv.reader(f)
+def read_file():
+    filename = 'aprio.csv'  
+    data = []
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
         for row in reader:
-            data.append(reader)
+            data.append(row)
     return data
 
-def get_freq_itemsets(data,min_support):
-    freq={}
-    for transactions in data:
-        for item in transactions:
-            freq[(item,)]=freq.get[(item,),0]+1
 
-    for itemset in combinations(transactions,2):
-        itemset=tuple(sorted(itemset))
-        freq[itemset]=freq.get[(itemset,),0]+1
+def get_freq_itemsets(data, min_support):
+    freq = {}
+    
+    
+    for transaction in data:
+        for item in transaction:
+            freq[(item,)] = freq.get((item,), 0) + 1
 
-    for itemset in combinations(transactions,3):
-        itemset=tuple(sorted(itemset))
-        freq[itemset]=freq.get[(itemset,),0]+1
+    
+    for transaction in data:
+        for itemset in combinations(transaction, 2):
+            itemset = tuple(sorted(itemset))
+            freq[itemset] = freq.get(itemset, 0) + 1
 
-    freq_item={}
-    for itemset,count in freq.item():
-        if count>+min_support:
-            freq_item[itemset]=count
-    freq=freq_item
+    
+    for transaction in data:
+        for itemset in combinations(transaction, 3):
+            itemset = tuple(sorted(itemset))
+            freq[itemset] = freq.get(itemset, 0) + 1
 
-    return freq
+   
+    freq_item = {}
+    for itemset, count in freq.items():
+        if count >= min_support:
+            freq_item[itemset] = count
+
+    return freq_item
 
 
-data=read_file('aprio.csv')
-min_support=2
-min_confidence=0.7
-frequent_itemsets=get_freq_itemsets(data[1:],min_support)
+data = read_file() 
+min_support = 2
+min_confidence = 0.7
+frequent_itemsets = get_freq_itemsets(data[1:], min_support) 
 
-print("frequent itemset")
-for itemset,count in frequent_itemset.item():
-    print(itemset,":",count)
+
+print("Frequent Itemsets:")
+for itemset, count in frequent_itemsets.items():
+    print(itemset, ":", count)
